9e2f7f351d515d79a51db2b43ef236b7bf79b362
algomachine007 <benneth@bejamas.io>
2022-09-14 21:15:38 +0100

    configuring absolute imports

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,12 +1,12 @@
 import { ReactNode } from 'react'
-
+import styles from './layout.module.scss'
 type Layout = {
   children: ReactNode
 }
 
 const Layout = ({ children }: Layout) => {
   return (
-    <main>{children}</main>
+    <main className={styles.wrapper}>{children}</main>
   )
 }
 
---
+++ components/top/index.tsx
@@ -0,0 +1,8 @@
+import styles from './top.module.scss'
+const Top = () => {
+  return (
+    <div className={styles.wrapper}>Top</div>
+  )
+}
+
+export default Top
