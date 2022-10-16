7d481b98b184067ae1ac36d661883d49632563dc
algomachine007 <benneth@bejamas.io>
2022-10-16 19:24:59 +0100

    updating font weight

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
@@ -6,14 +6,13 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Maitree:wght@400;600;700&family=Montserrat:ital,wght@0,400;0,600;0,700;0,900;1,400;1,600;1,900&family=Work+Sans:ital,wght@0,400;0,600;0,700;1,400&display=swap"
-            rel="stylesheet"
+            href='https://fonts.googleapis.com/css2?family=Maitree:wght@400;600;700&family=Montserrat:ital,wght@0,300;0,400;0,600;0,700;0,900;1,400;1,600;1,900&family=Work+Sans:ital,wght@0,300;0,400;0,600;0,700;1,400&display=swap'
+            rel='stylesheet'
           />
-
         </Head>
         <body>
           <Main />
-          < NextScript />
+          <NextScript />
         </body>
       </Html>
     );
