70ae459da1358b919242dbe7fe1a1e4cc19b3ee2
algomachine007 <benneth@bejamas.io>
2022-10-16 19:20:14 +0100

    positioning the components

--- components/bottom/index.tsx
+++ components/bottom/index.tsx
@@ -2,11 +2,19 @@ import styles from "./bottom.module.scss";
 import SectionWrapper from "@layout/sectionWrapper";
 import Banner from "@components/banner";
 import { IBottomProps } from "./types";
+import Image from "next/image";
 
 const Bottom = ({ info }: IBottomProps) => {
   return (
     <div className={styles.wrapper}>
-      <SectionWrapper>
+      <SectionWrapper withBg>
+        {/* <Image
+          src="/bailey.jpg"
+          className={styles.landingImage}
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center"
+        /> */}
         <Banner
           title={info.title}
           description={info.description}
--- components/layout/sectionWrapper/index.tsx
+++ components/layout/sectionWrapper/index.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import styles from './sectionWrapper.module.scss'
+import Image from 'next/image';
+import React from 'react';
+import styles from './sectionWrapper.module.scss';
 type SectionWrapper = {
-  children: React.ReactNode
-}
+  children: React.ReactNode;
+  withBg?: boolean;
+};
 
-const SectionWrapper = ({ children }: SectionWrapper) => {
-  return (
-    <div className={styles.wrapper}>{children}</div>
-  )
-}
+const SectionWrapper = ({ children, withBg }: SectionWrapper) => {
+  const className = withBg ? styles.wrapperWithBg : styles.wrapper;
+  return <div className={className}>{children}</div>;
+};
 
-export default SectionWrapper
+export default SectionWrapper;
